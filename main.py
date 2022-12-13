9b8d269b1ae861272c4c21d75daf8e7b10421bd1
AlekseyChurilov <118053184+AlekseyChurilov@users.noreply.github.com>
2022-12-13 07:04:24 +0300

    Update main.py
    Добавил пару примеров картинок

--- main.py
+++ main.py
@@ -11,7 +11,12 @@ classifier = pipeline("image-to-text", model="nlpconnect/vit-gpt2-image-captioni
 def root():
     """Эта функция вызывает классификатор"""
     return {"img":'https://img5.goodfon.ru/original/1920x1080/b/2f/tpvvvr-aprvvr-dublin.jpg'}
+
+#пара примеров картинок:
+#https://www.sunhome.ru/i/wallpapers/215/starinnii-zamok.1920x1080.jpg
+#https://phonoteka.org/uploads/posts/2022-09/1663900455_4-phonoteka-org-p-oboi-nochnogo-goroda-krasivo-4.jpg
+
 @app.post("/predict/")
 def predict(item: Item):
-    """генерируем описание рисунка"""
+    """Эта функция генерирует описание рисунка"""
     return classifier(item.text)[0]
